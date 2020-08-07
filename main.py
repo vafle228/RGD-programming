21a354c8d18322e9c29c5405d21feeb87d0d70a9
Neiro Fucker <dimasmyslaev228@gmail.com>
2020-08-07 16:45:44 +0500

    New "valid_input" and fixed bugs

--- main.py
+++ main.py
@@ -5,43 +5,31 @@ class Station:
     def __init__(self, ways):
         self.ways = []
         for i in range(ways):
-            while True:
-                try:
-                    capacity = int(input('Введите длину линии: '))
-                    if capacity < 0:
-                        print('Не понял, зачем отрицательные числа вводишь?')
-                        continue
-                    name = input('Введите название линии: ')
-                    self.ways.append(Way(capacity, name))
-                    break
-                except ValueError:
-                    print("Не понял, что началось то? Нормально общались!")
+            capacity = int(valid_input('Введите длину линии: ', 'int'))
+            name = input('Введите название линии: ')
+            self.ways.append(Way(capacity, name))
 
     def fill(self, train):
         s_ways = None
         for way in self.ways:
             if train.capacity <= way.capacity and way.filling is None \
                and (s_ways is None or s_ways.capacity > way.capacity):
-                s_ways.append(way)
+                s_ways = way
 
-        if len(s_ways):
-            _min = s_ways[0]
-            for s_way in s_ways:
-                if _min.capacity > s_way.capacity:
-                    _min = s_way
-            _min.filling = train
-            return 'Поезд {train_name} занял линию {name}'.format(name=_min.name,
-                                                                  train_name=_min.filling.name)
+            if s_ways is not None:
+                s_ways.filling = train
+                return 'Поезд {train_name} занял линию {name}'.format(name=s_ways.name,
+                                                                      train_name=s_ways.filling.name)
         return None
 
     def out_fill(self, train):
-        for i in range(len(self.ways)):
-            if self.ways[i].filling is None:
+        for way in self.ways:
+            if way.filling is None:
                 continue
-            elif self.ways[i].filling.name == train.name:
-                out = 'Поезд {train_name} покинул линию {name}'.format(name=self.ways[i].name,
-                                                                       train_name=self.ways[i].filling.name)
-                self.ways[i].filling = None
+            elif way.filling.name == train.name:
+                out = 'Поезд {train_name} покинул линию {name}'.format(name=way.name,
+                                                                       train_name=way.filling.name)
+                way.filling = None
                 return out
         return None
 
@@ -57,65 +45,55 @@ class Train:
     def __init__(self, name, capacity, arrive_time, out_time):
         self.capacity = int(capacity)
         self.name = name
-        self.arrive_time = time.strptime(arrive_time, '%H%M')
-        self.out_time = time.strptime(out_time, '%H%M')
+        self.arrive_time = arrive_time
+        self.out_time = out_time
+
+
+def valid_input(message, p_type):
+    while True:
+        data = input(message)
+        if p_type == 'int':
+            if data.isdigit() and int(data) >= 0:
+                return data
+            continue
+        if p_type == 'time':
+            try:
+                if ':' in data:
+                    data = data.split(':')[0] + data.split(':')[1]
+                return time.strptime(data, '%H%M')
+            except ValueError:
+                continue
 
 
 trains = []
 arrived_trains = []
-while True:
-    lines = input('Введите количество линий на станции: ')
-    if lines.isdigit():
-        break
-station = Station(int(lines))
 
-while True:
-    n = input('Сколько поездов подъезжает: ')
-    if not n.isdigit():
-        print('Не понял, что за бред ты мне ввел?')
-        continue
-    break
+station = Station(int(valid_input('Введите сколько линий: ', 'int')))
 
-for _ in range(int(n)):
+n = int(valid_input('Ввдети сколько поездов подъезжает: ', 'int'))
+
+for _ in range(n):
     train_name = input('Введите id поезда: ')
-    while True:
-        train_capacity = input('Введите длину поезда: ')
-        if train_capacity.isdigit():
-            if int(train_capacity) >= 0:
-                break
-    while True:
-        try:
-            train_arriving = input('Введите время прибытия: ')
-            if ':' in train_arriving:
-                train_arriving = train_arriving.split(':')[0] + train_arriving.split(':')[1]
-            time.strptime(train_arriving, '%H%M')
-            break
-        except ValueError:
-            continue
-    while True:
-        try:
-            train_outing = input('Введите время отбытия: ')
-            if ':' in train_outing:
-                train_outing = train_outing.split(':')[0] + train_outing.split(':')[1]
-            time.strptime(train_outing, '%H%M')
-            break
-        except ValueError:
-            continue
+    train_capacity = valid_input('Введите длинну поезда: ', 'int')
+    train_arriving = valid_input('Введите время прибытия: ', 'time')
+    train_outing = valid_input('Введите время отбытия: ', 'time')
+
     trains.append(Train(train_name, train_capacity, train_arriving, train_outing))
 
 trains = sorted(trains, key=lambda train: (train.out_time.tm_hour * 60 + train.out_time.tm_min) -
                                           (train.arrive_time.tm_hour * 60 + train.arrive_time.tm_min))
 
 while True:
-    for j in range(len(trains)):
-        if (time.strptime(time.strftime('%H%M'), '%H%M') == trains[j].arrive_time) and \
-                (trains[j] not in arrived_trains):
-            result = station.fill(trains[j])
+    for train in trains:
+        if (time.strptime(time.strftime('%H%M'), '%H%M') == train.arrive_time) and \
+           (train not in arrived_trains):
+            result = station.fill(train)
             if not result:
-                print('Поезд {} не смог заехать'.format(trains[j].name))
+                print('Поезд {} не смог заехать'.format(train.name))
+                trains.remove(train)
                 continue
             print(result)
-            arrived_trains.append(trains[j])
+            arrived_trains.append(train)
 
     for train in arrived_trains:
         if train in trains:
